03b985ad704750df995d8e8e03540ccb2db849b1
Louisa Gareiss <l.gareiss@icloud.com>
2024-06-29 10:44:54 +0200

    Initial Commit

---
+++ RippleEffectDemo/RippleEffectDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RippleEffectDemoApp.swift
+//  RippleEffectDemo
+//
+//  Created by Louisa Gareiss on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct RippleEffectDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
